573d05ae570389eea3ef52cc6c17e1da3c7caaa2
Mohamed <mouss.mo.ke@gmail.com>
2024-04-13 21:01:20 +0200

    working version

--- scraper.py
+++ scraper.py
@@ -1,10 +1,9 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import re
 import sqlite3
 import calendar
-
-#Looking for artist, genre, label, year of release, reviewer, review score, review date, review_length, best_new_music
+import time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 MONTH_TO_INDEX = dict((month, str(index)) for index, month in enumerate(calendar.month_abbr) if month)
 
@@ -25,10 +24,11 @@ def create_db(db_file_path):
         review_date_month INTEGER,
         review_date_year INTEGER,
         review_length INTEGER,
+        cover_url TEXT,
         PRIMARY KEY(album_name, artist, release_year)
     )"""
     db_cursor.execute(create_table_command)
-    return db_cursor
+    return db_connection, db_cursor
 
 def get_album_urls(page_soup):
     anchors = page_soup.find_all('a', href=re.compile("/reviews/albums/.+"))
@@ -112,15 +112,24 @@ def get_album_review_date(album_soup):
 def get_album_review_length(album_soup):
     try:
         review_divs = album_soup.find_all("div", class_="BodyWrapper-kufPGa cmVAut body body__container article__body")
-        review_word_count= sum(map(review_divs, lambda x: sum(x.get_text().split(' '))))
+        review_word_count = sum(map(lambda x: len(re.findall(r'\w+', x.get_text())), review_divs))
+        review_word_count = str(review_word_count)
     except :
         review_word_count= 'NULL'
     return review_word_count 
 
+def get_album_cover_url(album_soup):
+    try : 
+        cover_url_upper_div = album_soup.find('div', class_="SplitScreenContentHeaderLeadWrapper-jYfGAC lfiqNF")
+        cover_url = cover_url_upper_div.find('img')['src']   
+    except : 
+        cover_url = 'NULL'
+    return cover_url
+
 def get_album_data(album_soup):
     data = []
     funcs = [get_album_name, get_album_artist, get_album_genre, get_album_label, get_album_yor, get_album_score, get_album_bnm,
-            get_album_reviewer, get_album_review_date, get_album_review_length]
+            get_album_reviewer, get_album_review_date, get_album_review_length, get_album_cover_url]
     for func in funcs:
         if func == get_album_review_date:
             data.extend(func(album_soup))
@@ -128,31 +137,64 @@ def get_album_data(album_soup):
             data.append(func(album_soup))
     return data
 
-if __name__=='__main__':
-    db_file_path = "pitchfork_album_reviews.db"
-    db_cursor = create_db(db_file_path)
+def format_for_sql(data):
+    text_columns_indices = list(range(4)) + [7,12]
+    for i in text_columns_indices: 
+        if data[i] != "NULL":
+            if "'" in data[i]:
+                data[i] = data[i].replace("'", "''")
+            data[i] = "'" + data[i] + "'"
+    return data
 
+if __name__=='__main__':
+
+    # Initalizing database
+    db_file_path = "pitchfork_album_reviews.db"
+    db_connection, db_cursor = create_db(db_file_path)
+    insert_command = """INSERT INTO reviews (album_name,artist, genre, label,release_year, score, best_new_music, reviewer_name, review_date_day, review_date_month, review_date_year, review_length, cover_url) VALUES """
+
+    # Starting to scrape
     pitchfork_albums_review_url = "https://pitchfork.com/reviews/albums/?page={}"
-    pager = 1
-    albums_data = []
+    pager = 1 
+    n_reviews_counter = 0
     while(True):
+        # Opening page, fetching html data
         page_url = pitchfork_albums_review_url.format(str(pager))
         try:
+            time_i = time.time()
             page_html = urlopen(page_url).read().decode("utf-8")
+            time_j = time.time()
+            time_to_open_page = round((time_j - time_i)/1000, 3)
         except:
+            print("Scraping stopped when tried to fetch page number {} html data. Link : {}.".format(pager, page_url))
             break
+
+        print("Now processing page {}, which took {} seconds to open.".format(pager, round(time_to_open_page, 3)))
+
+        # Parsing html
         page_soup = BeautifulSoup(page_html, 'html.parser')
         album_urls = get_album_urls(page_soup)
         for album_url in album_urls:
             try:
+                time_i = time.time()
                 album_html = urlopen(album_url).read().decode("utf-8")
+                time_j = time.time()
+                time_to_open_page = round((time_j - time_i)/1000, 3)
             except:
                 continue
             album_soup = BeautifulSoup(album_html, 'html.parser')
             album_data = get_album_data(album_soup)
-            albums_data.append(album_data)
-        insert_albums_command = """INSERT INTO reviews VALUES """
-        commands = ','.join(["({})".format(','.join(album_data)) for album_data in albums_data])
-        final_command = insert_albums_command + commands
-        db_cursor.execute(final_command)
+            album_data = format_for_sql(album_data)
+            print("\tAlbum data retrieved from page {}, which took {} seconds to open.".format(album_url, time_to_open_page ))
+
+            # Inserting data in database
+            insert_values = "({})".format(','.join(album_data))
+            final_command = insert_command + insert_values
+            db_cursor.execute(final_command)
+
+        # Printing status
+        n_reviews_counter += len(album_urls)
+        print('\nThe database now contains {} reviews.\n'.format(n_reviews_counter))
         pager+=1
+
+    db_connection.close()
